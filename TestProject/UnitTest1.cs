b35a6c994ff23f8f55fe733539d19e924d8a489b
riseandcode1 <riseandcode@gmail.com1>
2015-02-08 14:58:16 +0200

    Added unit tests.

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+
+namespace TestProject
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+         var person = new Person("Sheldon");
+
+         Assert.AreEqual(person.Name, "Sheldon");
+      }
+   }
+}
